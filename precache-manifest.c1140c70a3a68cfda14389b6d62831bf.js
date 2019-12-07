072030f83ea894f4f5a460025fd2d1e39762cc9d
Faeghe <faeghe.hajiabadi@gmail.com>
2019-12-07 16:37:30 +0330

    Updates

---
+++ precache-manifest.c1140c70a3a68cfda14389b6d62831bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8104fde316367536a4f1bd9a9b675163",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "1b38345bc524b5fb2880",
+    "url": "/test/static/css/main.edd9e2d1.chunk.css"
+  },
+  {
+    "revision": "126a1ed39987d928ee5f",
+    "url": "/test/static/js/2.aaf7b422.chunk.js"
+  },
+  {
+    "revision": "1b38345bc524b5fb2880",
+    "url": "/test/static/js/main.27edaacd.chunk.js"
+  },
+  {
+    "revision": "a59cb56f235d3992a900",
+    "url": "/test/static/js/runtime~main.6dfd2fed.js"
+  }
+]);
